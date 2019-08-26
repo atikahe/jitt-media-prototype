8792024d314b32d96578111cd219b35e2c5bf832
atikahe <atikah.efnd@gmail.com>
2019-08-26 11:12:34 +0700

    first-commit

---
+++ js/side.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("navbarSupportedContent").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("navbarSupportedContent").style.width = "0";
+} 
